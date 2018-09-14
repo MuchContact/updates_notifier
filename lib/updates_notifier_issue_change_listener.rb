131eaf414f6b8e5cad3a2b8051e942abc36b4014
MuchContact <422549477@qq.com>
2018-09-14 16:03:03 +0800

    test log

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -7,9 +7,9 @@ class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
   def controller_issues_edit_after_save(context={})
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:issue] and context[:journal]
-        UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
+        logger.debug "webhook in update_notifier plugins"
+        # UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
       end
     end
   end
 end
-
