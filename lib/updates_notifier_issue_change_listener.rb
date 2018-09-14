17a7076b2df88c853802bf92f674c85864ebf95c
MuchContact <422549477@qq.com>
2018-09-14 15:37:43 +0800

    Revert "move code to lib"
    This reverts commit 316bb893cb4d1241fc40d267c3b59d128b3cf4ea.

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -1,6 +1,4 @@
 require 'rubygems'
-require 'httpclient'
-require 'json'
 
 class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
   def controller_issues_bulk_edit_after_save(context={})
@@ -9,34 +7,9 @@ class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
   def controller_issues_edit_after_save(context={})
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:issue] and context[:journal]
-        send_issue_update(User.current, context[:issue].id, context[:journal])
+        UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
       end
     end
   end
-  def self.send_issue_update(user, issueId, journal)
-    changes = []
-    journal.details.each do |j|
-      changes.push({
-        "property" => j.prop_key,
-        "value" => j.value
-      })
-    end
-    u = {"email" => user.mail, "firstname" => user.firstname, "lastname" => user.lastname}
-    post_to_server({
-        "type" => "issue",
-        "user" => u.to_json,
-        "issue" => issueId,
-        "comment" => journal.notes,
-        "changes" => changes.to_json,
-    })
-  end
-private
-  def self.post_to_server(data)
-    client = HTTPClient.new
-    client.debug_dev = STDOUT if $DEBUG
-    # RAILS_DEFAULT_LOGGER.debug("UPDATES_NOTIFIER: Posting update back to " + self.callback_url + ": " + data.to_json)
-    resp = client.post('http://192.168.101.19:8096/egova/code-review/statis/test', data)
-    # RAILS_DEFAULT_LOGGER.debug("UPDATES_NOTIFIER: Response code from " + self.callback_url + ": " + resp.status_code.to_s)
-    return resp
-  end
 end
+
